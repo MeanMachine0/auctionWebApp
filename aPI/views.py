fc13dd5747f3a6beaae872af5c418978b7ef6918
MeanMachineMarc <marcus9981@icloud.com>
2023-05-15 15:12:13 +0100

    data -> headers for getUser

--- aPI/views.py
+++ aPI/views.py
@@ -81,7 +81,7 @@ def getUsers(request):
 @api_view(["GET"])
 def getUser(request, pk):
     user = get_object_or_404(User, pk=pk)
-    if toBool[request.data['username']] == True:
+    if toBool[request.headers['username']] == True:
         return Response({"username": user.username})
     serializer = UserSerializer(user)
     return Response(serializer.data)
