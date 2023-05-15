308ae868a919ab85d99bdc329ca2a72d8adc5948
MeanMachineMarc <marcus9981@icloud.com>
2023-05-15 11:24:16 +0100

    removed unused login serializer

--- aPI/serializers.py
+++ aPI/serializers.py
@@ -21,7 +21,3 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = "__all__"
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
